afa7e1ac9151546ef51a22671e6bbb34a86e193e
velikhanov <teymurvk@mail.ru>
2021-04-07 21:02:08 +0400

    Update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,7 +36,7 @@ class AdminController extends Controller
 
       $user->name = $request->input('name')?$request->input('name'):Auth::user()->name;
       $user->email = $request->input('email')?$request->input('email'):Auth::user()->email;
-      $user->phone = $request->input('phone')?$request->input('phone'):(Auth::user()->phone?Auth::user()->phone:NULL);
+      $user->phone = $request->input('phone')?preg_replace('/\s+/', '', str_replace(array( '+', '-', '(', ')' ), '', $request->input('phone'))):(Auth::user()->phone?Auth::user()->phone:NULL);
 
       if ($request->hasFile('userimg')){
         Storage::disk('public')->exists('users/'.$user->img)?Storage::disk('public')->delete('users/'.$user->img):NULL;
